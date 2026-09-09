46cb0a8528ec2a77bea841d58721bc3341a2a9f8
rachit-developer24 <srachit841@gmail.com>
2026-09-09 02:16:21 +0100

    Initialize Spring Boot runbook assistant

---
+++ src/main/java/com/practice/netops/runbook/NetworkRunbookAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.practice.netops.runbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkRunbookAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkRunbookAssistantApplication.class, args);
+	}
+
+}
